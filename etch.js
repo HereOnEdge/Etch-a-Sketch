aba6549a75d66dd9f6e47d541b57030fbbb10873
Reza <reza1377maleki@gmail.com>
2022-09-11 18:57:11 +0430

    connect sizing range input to its function

--- etch.js
+++ etch.js
@@ -1,63 +1,85 @@
-const container = document.querySelector(".container");
-let pickedColor = document.querySelector("#color-picker")
+let container = document.querySelector(".container");
+let pickedColor = document.querySelector("#color-picker");
 let pickColorVal = pickedColor.value;
 let eraser = document.querySelector("#eraser");
 let clear = document.querySelector("#clear");
+let customRange = document.querySelector("#custom-size");
+let rangeOutput = customRange.value
+let rangeContainer = document.createElement("span")
+rangeContainer.classList.add("range-container");
+let label = document.querySelector("label")
+rangeContainer.textContent = `${rangeOutput}x${rangeOutput}`; 
+label.appendChild(rangeContainer);
+let mainHTMl = document.querySelector("main");
+
 
 xCustom = (x) => {
+    if(container.classList.value !== "container"){
+        console.log("its working")
+        container = document.createElement("div");
+        container.classList.add("container");
+        mainHTMl.insertBefore(container, document.querySelector(".options"))
+    } else {
+        console.log("not working")
+    }
+    let dimension = (100 / (x*x)) * x;
     for(let i = 0; i < x*x; i++ ){
         let piece = document.createElement('div');
         piece.classList.add("piece");
-        let dimension = (100 / (x*x)) * x;   
-        piece.style.cssText = `width: ${dimension}%` ; `height: ${dimension}%`;
+        piece.style.cssText = `width: ${dimension}%`;
         container.appendChild(piece);
     }
+    let pieces = document.querySelectorAll(".piece");
+    eraser.addEventListener("click",() => {
+        pieces.forEach((piece) => {
+            piece.addEventListener("mouseover" , () => {
+                piece.style.background = "#ffffff"           
+            })
+        })
+    })
+    pickedColor.addEventListener("change", () => {
+        pickColorVal = pickedColor.value;
+        pieces.forEach((piece) => {
+            piece.addEventListener("mouseover", () => {
+                piece.style.background = `${pickColorVal}`;
+            })
+        })
+    })
+    pickedColor.addEventListener("click", () => {
+        pickColorVal = pickedColor.value;
+        pieces.forEach((piece) => {
+            piece.addEventListener("mouseover", () => {
+                piece.style.background = `${pickColorVal}`;
+            })
+        })
+    })
+    
+    pieces.forEach(Draw = (piece) => {
+        piece.addEventListener('mouseover', () => {
+            piece.style.background = `${pickColorVal}`;
+        })
+    })
+    clear.addEventListener("click", () => {
+        pieces.forEach((piece) => {
+            piece.style.background = "#ffffff";
+        })
+    })
+    
+
+
 }
+xCustom(rangeOutput)
 
-xCustom(10)
-
-let pieces = document.querySelectorAll(".piece");
-
-eraser.addEventListener("click",() => {
-    pieces.forEach((piece) => {
-        piece.addEventListener("mouseover" , () => {
-            piece.style.background = "#ffffff"           
-        })
-    })
+customRange.addEventListener("change", () => {
+    rangeOutput = customRange.value;
+    rangeContainer.textContent = `${rangeOutput}x${rangeOutput}`;
+    container.remove();
+    container.classList.toggle("container")
+    console.log(container.classList)
+    xCustom(rangeOutput);
 })
-pickedColor.addEventListener("change", () => {
-    pickColorVal = pickedColor.value;
-    pieces.forEach((piece) => {
-        piece.addEventListener("mouseover", () => {
-            piece.style.background = `${pickColorVal}`;
-        })
-    })
-})
-pickedColor.addEventListener("click", () => {
-    pickColorVal = pickedColor.value;
-    pieces.forEach((piece) => {
-        piece.addEventListener("mouseover", () => {
-            piece.style.background = `${pickColorVal}`;
-        })
-    })
-})
-
-
-pieces.forEach(Draw = (piece) => {
-    piece.addEventListener('mouseover', () => {
-        piece.style.background = `${pickColorVal}`;
-    })
-})
-clear.addEventListener("click", () => {
-    pieces.forEach((piece) => {
-        piece.style.background = "#ffffff";
-    })
-})
+// customRange.addEventListener("click", xCustom(rangeOutput))
+// customRange.addEventListener("change", xCustom(rangeOutput))
 
 
 
-
-
-
-
-                                                 
